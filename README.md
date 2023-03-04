0e2b3abd0e6dcae5e6f171c4901df2eafcc97df8
Jeffry Luqman <jeffry.luqman@gmail.com>
2023-03-04 18:47:43 +0700

    init

---
+++ README.md
@@ -0,0 +1,31 @@
+# **Asset Management**
+Ini adalah aplikasi asset management sederhana, tugas Pemrograman 2 Kelompok 7 Kelas X2E
+
+## Anggota
+1. NPM - Abdur Rosyid Fachriansyah
+2. 202243570024 - [Jeffry Luqman](https://github.com/jeffry-luqman)
+3. NPM - Alfarobby
+4. NPM - Ahmad Badawi
+5. NPM - Sangga Buana
+6. NPM - Riyan Rizaldy
+
+## Memulai
+1. Pastikan komputer kamu sudah terinstall [Git](https://git-scm.com/) dan [Java Development Kit](http://jdk.java.net/).
+2. Kloning repositori ini ke komputer kamu dan masuk ke folder restoran dari terminal atau cmd
+	```bash
+	git clone https://github.com/pemrograman-unindra/p2-kelompok7.git && cd p2-kelompok7
+	```
+3. Lakukan kompilasi aplikasi
+	```bash
+	javac asset.java
+	```
+4. Jalankan aplikasi
+	```bash
+	java asset
+	```
+
+## Fitur
+- [ ] Melihat Data Asset
+- [ ] Menambah Data Asset
+- [ ] Merubah Data Asset
+- [ ] Menghapus Data Asset
---
+++ asset.java
@@ -0,0 +1,24 @@
+public class asset {
+
+	public static void main(String[] args) {
+		cetakJudul();
+	}
+
+	public static void cetakJudul() {
+		System.out.println();
+		System.out.println("+---------------------------------------+");
+		System.out.println("|            Asset Management           |");
+		System.out.println("+---------------------------------------+");
+		System.out.println("|          Tugas Pemrograman 2          |");
+		System.out.println("|               Kelompok 7              |");
+		System.out.println("|                                       |");
+		System.out.println("| NPM - Abdur Rosyid Fachriansyah       |");
+		System.out.println("| 202243570024 - Jeffry Luqman          |");
+		System.out.println("| NPM - Alfarobby                       |");
+		System.out.println("| NPM - Ahmad Badawi                    |");
+		System.out.println("| NPM - Sangga Buana                    |");
+		System.out.println("| NPM - Riyan Rizaldy                   |");
+		System.out.println("+---------------------------------------+");
+		System.out.println();		
+	}
+}
