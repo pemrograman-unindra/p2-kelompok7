dd690d7bb978d6df8294021bb9e351ab7db79160
Alfarobby27 <113989972+Alfarobby27@users.noreply.github.com>
2023-03-05 10:49:27 +0700

    Update asset.java

--- asset.java
+++ asset.java
@@ -6,19 +6,19 @@ public class asset {
 
 	public static void cetakJudul() {
 		System.out.println();
-		System.out.println("+---------------------------------------+");
-		System.out.println("|            Asset Management           |");
-		System.out.println("+---------------------------------------+");
-		System.out.println("|          Tugas Pemrograman 2          |");
-		System.out.println("|               Kelompok 7              |");
-		System.out.println("|                                       |");
-		System.out.println("| NPM - Abdur Rosyid Fachriansyah       |");
-		System.out.println("| 202243570024 - Jeffry Luqman          |");
-		System.out.println("| 202243500497 - Alfarobby              |");
-		System.out.println("| NPM - Ahmad Badawi                    |");
-		System.out.println("| NPM - Sangga Buana                    |");
-		System.out.println("| 202243500524 - Riyan Rizaldy          |");
-		System.out.println("+---------------------------------------+");
+		System.out.println("+------------------------------------------+");
+		System.out.println("|            Asset Management              |");
+		System.out.println("+------------------------------------------+");
+		System.out.println("|          Tugas Pemrograman 2             |");
+		System.out.println("|               Kelompok 7                 |");
+		System.out.println("|                                          |");
+		System.out.println("| 202243500501 - Abdur Rosyid Fachriansyah |");
+		System.out.println("| 202243570024 - Jeffry Luqman             |");
+		System.out.println("| 202243500497 - Alfarobby                 |");
+		System.out.println("| 202243500500 - Ahmad Badawi              |");
+		System.out.println("| 202243500502 - Sangga Buana              |");
+		System.out.println("| 202243500524 - Riyan Rizaldy             |");
+		System.out.println("+------------------------------------------+");
 		System.out.println();		
 	}
 }
